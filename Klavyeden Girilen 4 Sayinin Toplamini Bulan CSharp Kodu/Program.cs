e8b162c9bfee5768c48134a5e32a087020249e2f
yaren <yaren@DESKTOP-9U2BG79>
2024-10-18 01:57:14 +0300

    Add project files.

---
+++ Klavyeden Girilen 4 Sayinin Toplamini Bulan CSharp Kodu/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klavyeden_Girilen_4_Sayinin_Toplamini_Bulan_CSharp_Kodu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Konsolda sayıları direkt yanına yazmasını istediğim için WriteLine yerine Write yazdım.
+
+            Console.Write("Birinci Sayıyı Giriniz: ");
+            int Sayı1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("İkinci Sayıyı Giriniz: ");
+            int Sayı2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Üçüncü Sayıyı Giriniz: ");
+            int Sayı3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Dördüncü Sayıyı Giriniz: ");
+            int Sayı4 = Convert.ToInt32(Console.ReadLine());
+
+            int Toplam = Sayı1 + Sayı2 + Sayı3 + Sayı4;
+
+            Console.WriteLine("Girdiğiniz Dört Sayının Toplamı: " + Toplam);
+
+            Console.ReadKey();
+               
+        }
+    }
+}
---
+++ Klavyeden Girilen 4 Sayinin Toplamini Bulan CSharp Kodu/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Klavyeden Girilen 4 Sayinin Toplamini Bulan CSharp Kodu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Klavyeden Girilen 4 Sayinin Toplamini Bulan CSharp Kodu")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a26b6d75-5d85-4c33-9e46-baa2494d9ad2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
